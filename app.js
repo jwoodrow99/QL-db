e30ed1797165038ee6dc1841956d26b9a6ce84f6
Jack Woodrow <jackwoodrow@Jacks-MacBook-Pro.local>
2019-07-23 19:43:56 -0400

    changed git ignore and app.js demo code.

--- app.js
+++ app.js
@@ -1,24 +1,2 @@
+
 const db = require('./data/QL-db.js');
-
-const db1 = new db("db1");
-
-db1.newTable('friends', {
-    name:"string",
-    age:"number"
-});
-
-db1.insert('friends', [{
-    name:"John",
-    age:23
-},{
-    name:"Maddie",
-    age:25
-},{
-    name:"Kyle",
-    age:22
-},{
-    name:"Allison",
-    age:22
-}]);
-
-console.log(db1.find('friends', {}));
